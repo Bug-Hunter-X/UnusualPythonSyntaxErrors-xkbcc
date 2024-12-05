b2f5bba9f2b259c704afbd2aa0337108e6a24893
BugHunter-X <askzenai@gmail.com>
2024-12-05 14:04:06 +0100

    Add bugSolution.py file

---
+++ bugSolution.py
@@ -0,0 +1,8 @@
+def function(x):
+    return x + 1
+
+result = function(5) #Corrected function call with parenthesis
+print(result) # Output: 6
+
+result2 = function(5) #Corrected function call with parenthesis
+print(result2) # Output:6
